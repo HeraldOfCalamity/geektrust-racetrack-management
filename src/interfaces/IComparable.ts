33f8707beb75be8c6e033ace5effa2bb00ec6b84
Leonardo Eguino <releegva@gmail.com>
2024-07-28 23:41:09 -0400

    fix!: now types are exported at the modified interfaces.

--- src/interfaces/IComparable.ts
+++ src/interfaces/IComparable.ts
@@ -1,6 +1,6 @@
-type Comparator = 1 | 0 | -1;
+export type Comparator = 1 | 0 | -1;
 
-export default interface IComparable<T> {
+export interface IComparable<T> {
   isBetween(lowerBound: T, upperBound: T): boolean;
 
   equals(item: T): boolean;
