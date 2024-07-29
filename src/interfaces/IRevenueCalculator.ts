4890390d62fa5dd518276709a958c314e8506bb9
Leonardo Eguino <releegva@gmail.com>
2024-07-28 21:47:06 -0400

    fix: rollback to previous method signatures on IRevenueCalculator.ts

--- src/interfaces/IRevenueCalculator.ts
+++ src/interfaces/IRevenueCalculator.ts
@@ -2,9 +2,9 @@ import IRecord from './IRecord';
 import Track from './ITrack';
 
 export default interface IRevenueCalculator {
-  getBookingRevenue(records: IRecord[]): number;
+  _getBookingRevenue(records: IRecord[]): number;
 
-  getAdditionalRevenue(records: IRecord[]): number;
+  _getAdditionalRevenue(records: IRecord[]): number;
 
-  getTrackTotalRevenue(track: Track, records: IRecord[]): number;
+  _getTrackTotalRevenue(track: Track, records: IRecord[]): number;
 }
